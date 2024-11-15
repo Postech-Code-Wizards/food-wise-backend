99103cf4bf63c2d78812d9327be41b1d31c35afd
andy-spch <64855335+andy-spch@users.noreply.github.com>
2024-11-15 14:35:06 -0300

    Code1 - implementing project structure (#1)
    * first commit - initial project structure
    
    * code1-removed auto created compose yaml
    
    * feature/wise-01- added repositories and entities folder, removed password from userDTO
    
    ---------
    
    Co-authored-by: andy-spch <anderson.donaruma@gmail.com>

---
+++ foodWise/src/main/java/br/com/foodWise/foodWise/FoodWiseApplication.java
@@ -0,0 +1,13 @@
+package br.com.foodWise.foodWise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodWiseApplication.class, args);
+	}
+
+}
