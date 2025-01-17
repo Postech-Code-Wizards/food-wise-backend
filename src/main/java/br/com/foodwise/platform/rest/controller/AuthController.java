71bc7470080dc49d9affdd6452ed1bfae74e4818
Mateus <mateusbonet@yahoo.com.br>
2025-01-17 08:13:28 -0300

    feature/wise-0008- Implements API interface

--- src/main/java/br/com/foodwise/platform/rest/controller/AuthController.java
+++ src/main/java/br/com/foodwise/platform/rest/controller/AuthController.java
@@ -4,18 +4,12 @@ import br.com.foodwise.platform.model.entities.User;
 import br.com.foodwise.platform.rest.dtos.request.AuthRequest;
 import br.com.foodwise.platform.rest.dtos.response.AuthResponse;
 import br.com.foodwise.platform.service.TokenService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,46 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("api/v1/auth")
 @Tag(name = "Authentication", description = "Authentication crud controller")
-public class AuthController {
+public class AuthController implements AuthApi {
+
     private final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
 
-    @Operation(
-            description = "Authenticates the user in the system",
-            summary = "Authentication user",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "Ok",
-                            content = @Content(
-                                    mediaType = "application/json",
-                                    schema = @Schema(implementation = AuthResponse.class)
-                            )
-                    ),
-                    @ApiResponse(
-                            responseCode = "500",
-                            description = "Sorry, internal server error, try again later",
-                            content = @Content(
-                                    mediaType = "application/json",
-                                    examples = @ExampleObject(
-                                            """
-                                            {
-                                                "statusCode": 500,
-                                                "errors": [
-                                                    {
-                                                        "code": "INTERNAL_SERVER_ERROR",
-                                                        "message": "Sorry, internal server error, try again later"
-                                                    }
-                                                ]
-                                            }
-                                            """
-                                    )
-                            )
-                    )
-
-            }
-    )
-    @PostMapping("/login")
+    @Override
     public ResponseEntity<AuthResponse> login(@RequestBody @Valid AuthRequest request) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(request.email(), request.password());
         var auth = this.authenticationManager.authenticate(usernamePassword);
