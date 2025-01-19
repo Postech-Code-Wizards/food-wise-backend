60d7f18037a99c9f22a027165993bf883156d72f
Mateus <mateusbonet@yahoo.com.br>
2025-01-19 14:43:01 -0300

    bugfix/wise-0030- Do not allow deleted user to log in

--- src/main/java/br/com/foodwise/platform/rest/controller/AuthController.java
+++ src/main/java/br/com/foodwise/platform/rest/controller/AuthController.java
@@ -1,18 +1,22 @@
 package br.com.foodwise.platform.rest.controller;
 
 import br.com.foodwise.platform.model.entities.User;
+import br.com.foodwise.platform.rest.controller.exception.BusinessException;
 import br.com.foodwise.platform.rest.dtos.request.AuthRequest;
 import br.com.foodwise.platform.rest.dtos.response.AuthResponse;
 import br.com.foodwise.platform.service.TokenService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,6 +31,11 @@ public class AuthController implements AuthApi {
     public ResponseEntity<AuthResponse> login(@RequestBody @Valid AuthRequest request) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(request.email(), request.password());
         var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        var user = (User) auth.getPrincipal();
+        if (Objects.nonNull(user.getDeletedAt())) {
+            throw new BusinessException("USER_DOES_NOT_EXIST", HttpStatus.NOT_FOUND, "");
+        }
 
         var token = tokenService.generateToken((User) auth.getPrincipal());
 
