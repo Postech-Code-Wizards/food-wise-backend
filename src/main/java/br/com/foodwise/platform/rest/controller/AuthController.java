ae2a15b7833ff85ef66e4ca83bb0cbf3335ae1ea
mateusbonet <mateusbonet@yahoo.com.br>
2025-01-20 20:22:50 -0300

    bugfix/wise-0030- Do not allow deleted user to log in (#20)
    * bugfix/wise-0030- Do not allow deleted user to log in

--- src/main/java/br/com/foodwise/platform/rest/controller/AuthController.java
+++ src/main/java/br/com/foodwise/platform/rest/controller/AuthController.java
@@ -3,6 +3,7 @@ package br.com.foodwise.platform.rest.controller;
 import br.com.foodwise.platform.model.entities.User;
 import br.com.foodwise.platform.rest.dtos.request.AuthRequest;
 import br.com.foodwise.platform.rest.dtos.response.AuthResponse;
+import br.com.foodwise.platform.service.AuthService;
 import br.com.foodwise.platform.service.TokenService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/auth")
@@ -22,12 +24,14 @@ public class AuthController implements AuthApi {
 
     private final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
+    private final AuthService authService;
 
     @Override
     public ResponseEntity<AuthResponse> login(@RequestBody @Valid AuthRequest request) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(request.email(), request.password());
         var auth = this.authenticationManager.authenticate(usernamePassword);
 
+        authService.validateUserIsActive(auth);
         var token = tokenService.generateToken((User) auth.getPrincipal());
 
         return ResponseEntity.ok(new AuthResponse(token));
